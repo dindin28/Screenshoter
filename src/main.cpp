772c6b0705df46d56644f04204c31742fd3dda75
Dmytro Shtrikker <shtrikker28@gmail.com>
2023-08-17 19:18:48 +0300

    Added SQLite

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QScreen* screen = QApplication::primaryScreen();
-    QPixmap pixmap = screen->grabWindow(0);
-    pixmap.save("screenshot.png");
     w.show();
     return a.exec();
 }
